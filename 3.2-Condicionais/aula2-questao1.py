10fe38a17f633ffc9e0154964d4cafd324ede499
laissilvaap <lais.pereira@pdbomdespacho.com.br>
2025-02-10 17:02:52 -0300

    Delete 3.2-Condicionais directory

--- 3.2-Condicionais/aula2-questao1.py
+++
@@ -1,9 +0,0 @@
-#Entrada de dados
-idade_juliana = int(input("Digite a idade de Juliana:"))
-idade_cris = int(input("Digite a idade de Cris:"))
-
-#Processamento
-resultado = idade_juliana > 17 and idade_cris > 17
-
-#Saída de dados
-print (f"Podem entrar no bar? {resultado}")
