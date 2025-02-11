961f47495ac4522afbfc4a1d12b3881dbc25f2ba
laissilvaap <lais.pereira@pdbomdespacho.com.br>
2025-02-11 14:36:57 -0300

    Add files via upload

---
+++ modulo3/3.4-Condicionais/aula4-questao2.py
@@ -0,0 +1,11 @@
+avaliacao = int(input("Digite sua avaliação (de 1 a 5):"))
+if avaliacao == 5:
+    print ("Excelente!")
+if avaliacao == 4:
+    print ("Muito Bom!")
+if avaliacao == 3:
+    print ("Bom!")
+if avaliacao == 2:
+    print ("Regular.")
+if avaliacao == 1:
+    print ("Ruim.")
